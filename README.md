656bf14e5af56de3f5751dfcc4a7d058fc952439
Mark Harrison <markharrison@Marks-MacBook-Pro.local>
2018-10-14 21:06:13 +0100

    mod now uses array size

---
+++ README.md
@@ -0,0 +1,7 @@
+#Simple Bootstrap App
+
+This app uses Bootstrap and Flex to position a set of components.
+
+Hover animation occur via css transition.
+
+Colour changes occur via jQuery.
--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
 $(function() {
 
 	"use strict";
-	let colourArray = ['#FBFB9B', '#9BFBC7', '#9BD1FB'];
+	let colourArray = ['#FBFB9B', '#9BFBC7', '#9BD1FB', '#D60000'];
 	let clickCounter = 0;
 
-    console.log( "JS loaded" );
+    console.log( 'JS loaded');
 
 
     $('.text-box').click(function() {
 
     	clickCounter++;
-	 	changeColour(colourArray[clickCounter % 3]);
+	 	changeColour(colourArray[clickCounter % colourArray.length]);
 	});
 
 	let changeColour = (newColour) => {
 
 		$('.grid-element').css('background-color', newColour);
-	 	$('.text-box').css('color', newColour);
-	 	$('.text-box').css('border-color', newColour);
+	 	$('.text-box').css({'color': newColour, 'border-color': newColour });
 	}
 });
